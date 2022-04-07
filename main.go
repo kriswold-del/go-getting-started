b4f943e580c40d42bdc408154a1bd0fee57f874f
kriswold-del <83008723+kriswold-del@users.noreply.github.com>
2022-04-07 18:15:30 +0100

    Merge remote-tracking branch 'origin/main'

--- main.go
+++ main.go
@@ -43,9 +43,6 @@ func main() {
 		log.Fatal("$PORT must be set")
 	}
 
-
-
-
     p := bp3d.NewPacker()
     p.AddBin(bp3d.NewBin("Small Bin", 10, 15, 20, 100))
 	p.AddBin(bp3d.NewBin("Medium Bin", 100, 150, 200, 1000))
