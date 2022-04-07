7c71bc142444856157d42dfe35f2ad96c3cd8ab7
kriswold-del <83008723+kriswold-del@users.noreply.github.com>
2022-04-07 16:33:54 +0100

    updates

--- main.go
+++ main.go
@@ -1,18 +1,32 @@
 package main
 
 import (
+	"log"
 	"net/http"
+	"os"
     "fmt"
     "log"
 
     "github.com/gedex/bp3d"
+	"github.com/gin-gonic/gin"
+	_ "github.com/heroku/x/hmetrics/onload"
 )
 
 func main() {
-    http.HandleFunc("/", HelloServer)
-    http.ListenAndServe(":8080", nil)
-}
+	port := os.Getenv("PORT")
 
-func HelloServer(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
+	if port == "" {
+		log.Fatal("$PORT must be set")
+	}
+
+	router := gin.New()
+	router.Use(gin.Logger())
+	router.LoadHTMLGlob("templates/*.tmpl.html")
+	router.Static("/static", "static")
+
+	router.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "test", nil)
+	})
+
+	router.Run(":" + port)
 }
