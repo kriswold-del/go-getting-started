7391fa289172413483f7ff34aa3967c959784531
kriswold-del <83008723+kriswold-del@users.noreply.github.com>
2022-04-07 16:50:33 +0100

    updates

--- main.go
+++ main.go
@@ -17,6 +17,19 @@ func main() {
 		log.Fatal("$PORT must be set")
 	}
     p := bp3d.NewPacker()
+p.AddBin(bp3d.NewBin("Small Bin", 10, 15, 20, 100))
+	p.AddBin(bp3d.NewBin("Medium Bin", 100, 150, 200, 1000))
+
+	// Add items.
+	p.AddItem(bp3d.NewItem("Item 1", 2, 2, 1, 2))
+	p.AddItem(bp3d.NewItem("Item 2", 3, 3, 2, 3))
+
+	// Pack items to bins.
+	if err := p.Pack(); err != nil {
+		log.Fatal(err)
+	}else{
+	log.info(p.Bins)
+	}
 
 	router := gin.New()
 	router.Use(gin.Logger())
